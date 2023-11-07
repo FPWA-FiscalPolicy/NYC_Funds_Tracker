735931d11c3236883f3d2dabef144419e5f0becf
Zoey Lyu <95316737+ZoeyyyLyu@users.noreply.github.com>
2023-11-07 14:28:48 -0500

    Update Merge.R

--- Code/Merge data/Merge.R
+++ Code/Merge data/Merge.R
@@ -1,14 +1,38 @@
 # This script can merge the raw datasets into one file.
+# Read me before merging: 
+   #1. confirm the url link below is correct
+   #2. confirm the agency code list is correct
+
 library(RCurl)
 library(tidyverse)
-# Revenue
-# Budget Expenditure
 agencies_code <- c('068','260','125','069','806','071','816','801',"040")
+# Revenue
+
+url<- "https://raw.githubusercontent.com/ZoeyyyLyu/NYC_Fund_Tracker/main/Raw%20Data/Raw%20Data%20-%20Revenue/"
+for (i in agencies_code){
+  filename <- paste0("Revenue_",i,"_all.csv")
+  c_url <- paste0(url,filename)
+  if (i==1){
+    raw_budget <- read.csv(c_url)
+  } else {
+    current_data <- read.csv(c_url)
+    raw_budget <- rbind(raw_budget,current_data)
+  }
+}
+write.csv(raw_budget,"~/Desktop/raw_revenue.csv")
+
+
+# Budget Expenditure
 url<- "https://raw.githubusercontent.com/ZoeyyyLyu/NYC_Fund_Tracker/main/Raw%20Data/Raw%20Data%20-%20Budget/"
 for (i in agencies_code){
   filename <- paste0("budget_",i,"_all.csv")
   c_url <- paste0(url,filename)
-  print(c_url)
-  #currentfile <-read.csv(c_url)
+  if (i==1){
+    raw_budget <- read.csv(c_url)
+  } else {
+    current_data <- read.csv(c_url)
+    raw_budget <- rbind(raw_budget,current_data)
+  }
 }
+write.csv(raw_budget,"~/Desktop/raw_budget.csv")
 
