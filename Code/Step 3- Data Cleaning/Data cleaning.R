ce632cc19c94cfd770d7e70ea3492d89a699f7e9
Zoey Lyu <95316737+ZoeyyyLyu@users.noreply.github.com>
2023-11-15 14:33:35 -0500

    data cleaning -update

--- Code/Step 3- Data Cleaning/Data cleaning.R
+++ Code/Step 3- Data Cleaning/Data cleaning.R
@@ -12,7 +12,7 @@ Revenue_data <- fread('~/Desktop/Merged_data/raw_revenue.csv') # # of obs= 11037
 agency_code_dataframe <- data.frame(agency=unique(Revenue_data$agency),
                                     agency_code=c(c('068','260','125','069','806','071','816','801',"040")))
 Revenue_data <- left_join(Revenue_data,agency_code_dataframe,by="agency")
-years <- c(2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022,2023)
+
 # Intra-city sales
 intra_city_sales <- Revenue_data %>%
   filter(closing_classification_name != "Collected Unearned Revenue Roll" &
@@ -22,22 +22,35 @@ intra_city_sales <- Revenue_data %>%
   summarise(intracity_sales=sum(recognized,na.rm=T))
 colnames(intra_city_sales)[2] <- c("year")
 
+# clean the raw Revenue_data
+  # 1. Delete: closing_classification_name= "Collected Unearned Revenue Roll" / "Accounts Left In Old Year";
+  # 2. Remove funding class = "INTRA-CITY SALES" / "CAPITAL FUNDS - I.F.A."
 Revenue_data <- Revenue_data %>%
   filter(closing_classification_name != "Collected Unearned Revenue Roll" &
            closing_classification_name != "Accounts Left In Old Year")  %>%
   filter(funding_class !="INTRA-CITY SALES")%>%
   filter(funding_class !="CAPITAL FUNDS - I.F.A.")%>%
-  filter(fiscal_year<=2023 & fiscal_year>=2011) 
+  filter(fiscal_year<=2023 & fiscal_year>=2011) %>%
+  select(- budget_fiscal_year, -V1, -X)
+
+# add a column --  total revenue for every agency  
+agency_revenue <- Revenue_data %>%
+  group_by(agency_code,fiscal_year)%>%
+  summarize(agency_recog_rev=sum(recognized,na.rm=T))
 
 #Budget
 Budget_data <- fread('~/Desktop/Merged_data/raw_budget.csv')
 Budget_data <- left_join(Budget_data,agency_code_dataframe,by="agency")
 
 #‘Committed’ - Sum of ‘Pre-Encumbered’ , ‘Encumbered’ , ‘Accrued Expense’, ‘Cash Payments’ and ‘Post Adjustments’.
-# Actual Expenditure= Committed budget + Cash expense
+# Actual Expenditure= Committed budget + Cash expense 
 Budget_data <- Budget_data %>%
   mutate(actual_expenditure=pre_encumbered+encumbered+accrued_expense+post_adjustment+cash_expense)
 
+# add a column --  total budget for every agency  
+# total budget expenditure for the agency=  sum(Actual Expenditure)- intra city sales- prior payable
+Prior_payable <- fread("~/Desktop/")
+  
 agency_Budget_summary <- Budget_data %>%
   group_by(agency_code,year)%>%
   summarise(agency_total_budget=sum(actual_expenditure,na.rm=T))
--- Code/Step 3- Data Cleaning/Read me before cleaning the data.md
+++ Code/Step 3- Data Cleaning/Read me before cleaning the data.md
@@ -5,12 +5,16 @@
 2. Remove funding class = "INTRA-CITY SALES" / "CAPITAL FUNDS - I.F.A."
 
 # Budget
-1. the Actual budget expenditure=" Commited" +"Cash expense", where ‘Committed’= Sum of ‘Pre-Encumbered’ , ‘Encumbered’ , ‘Accrued Expense’, ‘Cash Payments’ and ‘Post Adjustments’. 
+1. $Actual \ Budget\  Expenditure= Commited +Cash \ Expense$, \
+where $Committed= Pre\ Encumbered+Encumbered+Accrued \ Expense+Cash \ Payments+Post \ Adjustments$. 
 
-2. The figrues of "Net Change in Estimates of
+2. $Agency \ Budget \ Expenditure =  sum(Actual \ Budget\ Expenditure)- Intracity \ Sales - Prior \ Payables$
+
+3. The figrues of "Net Change in Estimates of
 Prior Payables" in the Comptroller’s Report—General Fund—Schedule G5 can be found in the Raw data - "Prior_Payable.csv". The figures are hand collected and need to mannually update in the .csv file every year. 
+
+4. Comptroller’s Report—General Fund—Schedule G5 has "_Intra-city sales_", which can be calculated by summing figures in column "_recognized_" across all records with funding class= "INTRA-CITY SALES" in the revenue raw data
 
 # Notes:
 1. Comptroller’s Report —General Fund—Schedule G3 has "Net Change in Estimate of Prior Receivables", which can be calculated by summing figures in column "recognized" across all records with [ closing_classification_name= "Accounts Left In Old Year" & funding_class !="INTRA-CITY SALES" &  funding class != "CAPITAL FUNDS - I.F.A."]
 
-2. Comptroller’s Report—General Fund—Schedule G5 has "intra-city sales", which can be calculated by summing figures in column "recognized" across all records with funding class= "INTRA-CITY SALES" in the revenue raw data
