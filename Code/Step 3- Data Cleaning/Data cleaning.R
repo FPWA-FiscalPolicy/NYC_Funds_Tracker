4526fd984c030dbbad46703f0921bbdf0354991a
Zoey Lyu <95316737+ZoeyyyLyu@users.noreply.github.com>
2023-11-15 11:44:22 -0500

    update ACFR data sheets

--- Code/Step 3- Data Cleaning/Data cleaning.R
+++ Code/Step 3- Data Cleaning/Data cleaning.R
@@ -11,29 +11,36 @@ Revenue_data <- fread('~/Desktop/Merged_data/raw_revenue.csv') # # of obs= 11037
 #construct a agency-code dictionary.
 agency_code_dataframe <- data.frame(agency=unique(Revenue_data$agency),
                                     agency_code=c(c('068','260','125','069','806','071','816','801',"040")))
+Revenue_data <- left_join(Revenue_data,agency_code_dataframe,by="agency")
+years <- c(2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022,2023)
+# Intra-city sales
+intra_city_sales <- Revenue_data %>%
+  filter(closing_classification_name != "Collected Unearned Revenue Roll" &
+           closing_classification_name != "Accounts Left In Old Year")  %>%
+  filter(funding_class=="INTRA-CITY SALES")%>%
+  group_by(agency_code,fiscal_year)%>%
+  summarise(intracity_sales=sum(recognized,na.rm=T))
+colnames(intra_city_sales)[2] <- c("year")
+
 Revenue_data <- Revenue_data %>%
   filter(closing_classification_name != "Collected Unearned Revenue Roll" &
            closing_classification_name != "Accounts Left In Old Year")  %>%
   filter(funding_class !="INTRA-CITY SALES")%>%
   filter(funding_class !="CAPITAL FUNDS - I.F.A.")%>%
   filter(fiscal_year<=2023 & fiscal_year>=2011) 
-Revenue_data <- left_join(Revenue_data,agency_code_dataframe,by="agency")
-
 
 #Budget
 Budget_data <- fread('~/Desktop/Merged_data/raw_budget.csv')
 Budget_data <- left_join(Budget_data,agency_code_dataframe,by="agency")
 
 #‘Committed’ - Sum of ‘Pre-Encumbered’ , ‘Encumbered’ , ‘Accrued Expense’, ‘Cash Payments’ and ‘Post Adjustments’.
-# Actual Expenditure=Committed budget + Cash expense
+# Actual Expenditure= Committed budget + Cash expense
 Budget_data <- Budget_data %>%
   mutate(actual_expenditure=pre_encumbered+encumbered+accrued_expense+post_adjustment+cash_expense)
 
-#Budget
-Revenue_data <- fread('~/Desktop/Merged_data/raw_revenue.csv')
-Revenue_data %>%
-  filter(closing_classification_name != "Collected Unearned Revenue Roll" &
-           closing_classification_name != "Accounts Left In Old Year")  %>%
-  filter(funding_class=="INTRA-CITY SALES")%>%
-  group_by(fiscal_year,agency)%>%
-  summarise(sum_recog=sum(recognized,na.rm=T))
+agency_Budget_summary <- Budget_data %>%
+  group_by(agency_code,year)%>%
+  summarise(agency_total_budget=sum(actual_expenditure,na.rm=T))
+agency_Budget_summary <- left_join(agency_Budget_summary,intra_city_sales,by=c("agency_code","year"))
+agency_Budget_summary$agency_total_budget <- agency_Budget_summary$agency_total_budget-agency_Budget_summary$intracity_sales
+
